45f4f3187c0ba5ce6652269520dac84d25b2dc1e
hafizpatwary <hafiz9711@gmail.com>
2019-11-26 14:14:15 +0000

    adjusted spelling mistake

--- testing/factorial_test.py
+++ testing/factorial_test.py
@@ -1,5 +1,5 @@
 import pytest
-from python_execricses import factorial
+from python_exercise import factorial
 
 def test_factorial():
 	assert factorial.factorial(120) == 5, "failed zero factorial"
